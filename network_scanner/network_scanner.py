394be83d1ef8cb724302987216e554ca5e119912
Maksym Lytvynchuk <lytvynchuk.official@gmail.com>
2019-08-02 12:31:54 +0300

    get mac from ip

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -1,7 +1,10 @@
 import scapy.all as scapy
-
+import os
 def scan(ip):
-    scapy.arping(ip)
-
+    arp_request = scapy.ARP(pdst = ip) # get an ip of our computer
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff") # get a mac address of our computer
+    arp_request_brodcast = broadcast/arp_request # arp_request + broadcast
+    answered, unanswered = scapy.srp(arp_request_brodcast, timeout=1)
+    print(answered.summary())
 
 scan("10.0.2.1/24")
